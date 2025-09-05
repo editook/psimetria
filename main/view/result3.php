9365566e7d11628d31687b9ad344b01dbe5ecc6f
edito <edito.ok@gmail.com>
2025-09-05 19:22:04 -0400

    fix version code

--- main/view/result3.php
+++ main/view/result3.php
@@ -1759,7 +1759,7 @@ if($answer_questions2['response'] == '1'){
                     url("https://db.onlinewebfonts.com/t/5f9ecd69838280dcd8a9f0072f92f6a6.woff") format("woff");
                 }
         </style>
-        <link href="../../assets/css/style_profile.css?v=1.0.4" rel="stylesheet">
+        <link href="../../assets/css/style_profile.css?v=<?=VERSION_CODE?>" rel="stylesheet">
 	</head>
 
 	<body class="main-body">
@@ -1794,8 +1794,8 @@ if($answer_questions2['response'] == '1'){
 								<div class="card-body">
                                     <div class="row row-sm">
                                         <div class="col-12 col-md-3 col-lg-2 img-container">
-                                            <img alt="" class="float-sm-right wd-100p mg-sm-t-0 img-logo"  src="../../assets/img/test_image/perfil-sf.png">
-                                            <img alt="" class="float-sm-right wd-100p mg-sm-t-0 img-logo"  src="../../assets/img/test_image/logomaci.jpeg">
+                                            <img alt="" class="float-sm-right wd-100p mg-sm-t-0 img-logo"  src="../../assets/img/test_image/perfil-sf.png?v=<?=VERSION_CODE?>">
+                                            <img alt="" class="float-sm-right wd-100p mg-sm-t-0 img-logo"  src="../../assets/img/test_image/logomaci.jpeg?v=<?=VERSION_CODE?>">
                                         </div>
                                         <div class="col-12 col-md-9 col-lg-10">
                                         <div class="row">
@@ -2346,8 +2346,8 @@ if($answer_questions2['response'] == '1'){
 		<script src="../../assets/plugins/sidebar/sidebar-custom.js"></script>
 
 		<!-- custom js -->
-		<script src="../../assets/js/custom.js"></script>
-        <script src="../../assets/js/print.js"></script>
+		<script src="../../assets/js/custom.js?v=<?=VERSION_CODE?>"></script>
+        <script src="../../assets/js/print.js?v=<?=VERSION_CODE?>"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/html2canvas/1.4.1/html2canvas.min.js"></script>
         <script>
             $(function() {
